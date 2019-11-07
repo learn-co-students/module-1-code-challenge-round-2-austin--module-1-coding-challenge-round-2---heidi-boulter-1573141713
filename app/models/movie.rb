9f047d02573e08969543cf47943262cb3f9ac2d3
Heidi Boulter <boulter.heidi@gmail.com>
2019-11-07 12:49:31 -0600

    submission

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,28 @@ class Movie
     @@all
   end
 
+  def reviews
+# returns an array of all the Review instances for the Movie
+    Review.all.select { |reviews| reviews.movie == self }
+  end
+
+  def reviewers
+# returns an array of all of the Viewer instances that reviewed the Movie
+    review_arr = self.reviews
+    review_arr.map { |reviews| reviews.viewer }
+  end
+
+  def average_rating
+# returns the average of all ratings for the Movie instance
+# to average ratings, add all ratings together and divide by the total number of ratings
+    ratings_arr = reviews
+    ratings_arr.map { |reviews| reviews.rating }.reduce(0) { |sum, num| sum + num }.to_f / ratings_arr.length
+  end
+
+  def highest_rated
+# returns the Movie instance with the highest average rating.
+# BONUS
+    average_rating
+  end
+
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,29 @@ class Viewer
   def self.all
     @@all
   end
+
+  def reviews
+# returns an array of Review instances associated with the Viewer instance
+    Review.all.select { |reviews| reviews.viewer == self }
+  end
+
+  def reviewed_movies
+# returns an array of Movie instances reviewed by the Viewer instance.
+    review_arr = self.reviews
+    review_arr.map { |reviews| reviews.movie }
+  end
+
+  def reviewed_movie?(movie)
+    reviewed_movies.include?(movie)
+  end
+
+  def rate_movie(movie, rating)
+# ???
+    if !reviewed_movies.include?(movie) # "!" because does not associate?
+      Review.new
+    else 
+      @rating = rating.keep_if { |rating| rating < 10 } # ratings only up to 10
+    end
+  end
   
 end
